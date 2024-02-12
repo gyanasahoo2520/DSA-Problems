192bdd26ab1d5327ec21a2e5d2a45b33b847c6c3
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-02-12 19:50:50 +0530

    recursion commit

--- src/DSA_Problems/Arrays/Recursion/MergeSort.java
+++ src/DSA_Problems/Arrays/Recursion/MergeSort.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-        int[] ans=mergeSort(arr);
-        System.out.println(Arrays.toString(ans));
+//        int[] ans=mergeSort(arr);
+//        System.out.println(Arrays.toString(ans));
+//        System.out.println(Arrays.toString(arr));
+
+        mergeSortInPlace(arr,0,arr.length);
         System.out.println(Arrays.toString(arr));
     }
 // In this function a copy of object is created instead of changing that object
@@ -47,5 +50,44 @@ public class MergeSort {
             k++;
         }
         return mix;
+    }
+    static void mergeSortInPlace(int[] arr,int s,int e){
+        if(e-s==1)
+            return;
+        int mid=(s+e)/2;
+        mergeSortInPlace(arr,s,mid);
+        mergeSortInPlace(arr,mid,e);
+
+        mergeInPlace(arr,s,mid,e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int mid, int e) {
+        int[] mix=new int[e-s];
+        int i=s,j=mid,k=0;
+        while(i<mid && j<e){
+            if(arr[i]<arr[j]){
+                mix[k]=arr[i];
+                i++;
+            }else{
+                mix[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < mid) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j<e){
+            mix[k]=arr[j];
+            j++;
+            k++;
+        }
+        for(int l=0;l<mix.length;l++){
+            arr[s+l]=mix[l];
+        }
     }
 }
