ab7a085bb5be2024e6390218746f3c9491c36a37
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2024-08-19 16:49:50 +0530

    recursion commit

--- src/DSA_Problems/String/ReverseStr.java
+++ src/DSA_Problems/String/ReverseStr.java
@@ -2,6 +2,11 @@ package DSA_Problems.String;
 
 public class ReverseStr {
     public static void main(String[] args) {
-
+        String name="javatpoint";
+        String temp="";
+        for(int i=0;i<name.length();i++){
+            temp=name.charAt(i)+temp;
+        }
+        System.out.println(temp);
     }
 }
