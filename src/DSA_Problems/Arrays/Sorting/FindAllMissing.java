11d582f6da514dbbb3532dca52a6834f9eabfa3c
gyanasahoo2520 <gyanaranjan2520@gmail.com>
2025-01-26 08:10:48 +0530

    recursion commit

--- src/DSA_Problems/Arrays/Sorting/FindAllMissing.java
+++ src/DSA_Problems/Arrays/Sorting/FindAllMissing.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class FindAllMissing {
     public static void main(String[] args) {
-        int[] arr={4,3,2,7,8,2,3,1};
+        int[] arr={4,3,2,4,8,2,3,1};
         System.out.println(findAllMissing(arr));
     }
     public static ArrayList<Integer> findAllMissing(int[] arr){
